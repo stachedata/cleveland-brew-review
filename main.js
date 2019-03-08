5f2de23b1b2f3aec2fb136ab5ac818ebfdd6f22e
stachedata <stachedata@gmail.com>
2019-03-08 16:15:21 -0500

    sort and print brews by rating

--- main.js
+++ main.js
@@ -11,16 +11,40 @@ const newBrew = (cafe,where,drink,rating,info) => {
     brews.push(brew)
 } 
 
-const printTopBrew = () => {
+const printBrews = () => {
+    brews.sort((a,b) => b.rating - a.rating)
+
     brews.map(brew => {
-        if(brew.rating >= 3) {
-            document.getElementById('topBrew').innerHTML = brew.cafe + '<br>' + "&#9733" + "&#9733"+ "&#9733  " + brew.drink 
+        const brewDiv = document.createElement('div')
+        brewDiv.className = 'brew'
+
+        if(brew.rating == 3) {
+            if(document.getElementById('topBrew').innerHTML == ""){
+                document.getElementById('topBrew').innerHTML = brew.cafe + "<br> &#9733 &#9733 &#9733  " + brew.drink 
+            }else{
+                brewDiv.innerHTML = brew.cafe + "<br> &#9733 &#9733 &#9733  " + brew.drink
+                document.getElementById('brews').appendChild(brewDiv)
+            }
+        }
+        else if(brew.rating == 2){
+            brewDiv.innerHTML = brew.cafe + "<br> &#9733 &#9733 &#9734  " + brew.drink
+            document.getElementById('brews').appendChild(brewDiv)
+        }
+        else{
+            brewDiv.innerHTML = brew.cafe + "<br> &#9733 &#9734 &#9734  " + brew.drink
+            document.getElementById('brews').appendChild(brewDiv)
         }
     })   
 }
 
+
 newBrew('Rising Star', "Lakewood", 'Iced Mocha', 3, "Super good.")
-newBrew("Pheonix", "Cleveland", "Hot Mocha", 2, "It's ok.")
+newBrew("Pour", "Cleveland", "Not Mocha", 1, "It's ok.")
+newBrew("Pheonix", "Cleveland", "Hot Mocha", 3, "Very nice.")
+newBrew("Duck Rabbit", "Cleveland", "Espresso", 2, "")
+newBrew("Rising Star", "Lakewood", "Coffee", 2, "")
+newBrew("7-Eleven", "Lakewood", "Coffee", 1, "")
+newBrew("QuickStop", "Cleveland", "Coffee", 1, "")
 
 
-printTopBrew()
+printBrews()
